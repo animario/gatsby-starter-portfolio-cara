5d9c4f95e1aaa540a945f0d949e248b7e2fb9698
Domo <domoontheblock@gmail.com>
2020-09-14 22:33:13 +0200

    test

--- src/@lekoarts/gatsby-theme-cara/components/layout.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/layout.tsx
@@ -22,6 +22,7 @@ const Layout = ({ children, className = `` }: LayoutProps) => (
         html: {
           fontSize: `18px`,
           WebkitTextSizeAdjust: `100%`,
+          text-align: `center`,
         },
         img: {
           borderStyle: `none`,
