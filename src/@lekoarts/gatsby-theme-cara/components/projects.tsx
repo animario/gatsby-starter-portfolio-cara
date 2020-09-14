2c7314d02124e901c03b1f5f256c6c5dfbc0656c
Domo <domoontheblock@gmail.com>
2020-09-14 22:11:54 +0200

    test

--- src/@lekoarts/gatsby-theme-cara/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/projects.tsx
@@ -10,13 +10,6 @@ import ProjectsMDX from "../sections/projects"
 
 const Projects = ({ offset, factor = 2 }: { offset: number; factor?: number }) => (
   <div>
-    <Divider
-      bg="linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%)"
-      sx={{ clipPath: `polygon(0 15%, 100% 25%, 100% 85%, 0 75%)` }}
-      speed={-0.2}
-      offset={1.1}
-      factor={factor}
-    />
     <Content speed={0.4} offset={offset + 0.2} factor={factor}>
       <Inner>
         <div
