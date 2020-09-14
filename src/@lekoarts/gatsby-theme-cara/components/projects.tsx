cae4b46f3f23b36edfd1cd3b7b5a4cf91405e035
Domo <domoontheblock@gmail.com>
2020-09-14 03:23:39 +0200

    test

--- src/@lekoarts/gatsby-theme-cara/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/projects.tsx
@@ -23,7 +23,7 @@ const Projects = ({ offset, factor = 2 }: { offset: number; factor?: number }) =
           sx={{
             display: `grid`,
             gridGap: [4, 4, 4, 5],
-            gridTemplateColumns: [`1fr`, `1fr`, `repeat(2, 1fr)`],
+            gridTemplateColumns: [`1fr`, `1fr`, `repeat(2, 1fr)`]
           }}
         >
           <ProjectsMDX />
